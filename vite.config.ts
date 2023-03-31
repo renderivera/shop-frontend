17f3e5ae2ae82f1b2cb2656c9ff98d3acbbe5128
raunchard <reinhard.zach@gmail.com>
2023-03-31 11:54:29 +0300

    add http server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		viteCompression({ algorithm: "brotliCompress" }),
+		viteCompression({ algorithm: "gzip" }),
+	],
+});
